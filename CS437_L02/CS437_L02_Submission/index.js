ef4e0d7af57b82e2285e652b77c2838efc6f49a2
Damon Liu <minqliu@microsoft.com>
2025-10-17 19:44:36 -0700

    update for bule tooth connection

--- CS437_L02/CS437_L02_Submission/index.js
+++ CS437_L02/CS437_L02_Submission/index.js
@@ -1,9 +1,25 @@
+/*	Minqiang Liu (mliu110), Yi Zhang (yjzhang2)
+ *	index.js
+ *	20251014
+ *	Significant refactoring from initial `index.js` so that the codebase is
+ *	more friendly to toggle between two potential communication methods.
+ */
+
 const net = require('net');
+const assert = require('assert');
+const bt = require('bluetooth-serial-port');	// for bluetooth
+const BLUETOOTH_MAC_ADDRESS = '';
 
 // -------------------- Configuration --------------------
 const server_addr = "192.168.0.42";   // Raspberry Pi IP Address
-const server_port = 5000;             // TCP port
+const server_port = 5000;
+// TCP port
+const CONNECTION_TYPE_ENUM = new Set([
+    "ip",
+    "bluetooth"
+]);
 
+const CONNECTION_TYPE = "ip";
 // -------------------- Global state --------------------
 let client_socket1 = null;
 let connect_Flag = false;
@@ -15,6 +31,78 @@ let elBluetooth, elDir, elObstacle, elTemperature, elCpuLoad;
 // -------------------- Servo Sweep State --------------------
 let servoAngle = 45;
 let servoIncreasing = true;
+
+//	-------------------- NETWORK LOGIC
+/**	bt_connect()
+ *	Method that attempts to connect by bt.
+ *	Follows `bluetooth-serial-port` pkg documentation (https://www.npmjs.com/package/bluetooth-serial-port)
+ *	but assumes MAC address is already known to prevent stray connection.
+ *
+ *	TODO: Refactor into `socket_connect()` to build a general `connect()`
+ *	method; IDEA: return `client_obj` that is returned by `bt_connect()`/
+ *	`socket_connect()` afterwards.
+ *	TODO: figure out an `async` friendly library to prevent callback hell within
+ *	`findSerialPortChannel()`, etc.
+ *	ALT BT BY ELECTRONJS IDEA: https://www.electronjs.org/docs/latest/tutorial/devices
+ *
+ *	@param {String} target_mac_address - Device MAC address string
+ *	@param {int} target_channel - Device channel; OPTIONAL
+ *	@returns {null}
+ */
+function bt_connect(target_mac_address, target_channel) {
+    client_obj = new bt.BluetoothSerialPort();
+    // https://www.npmjs.com/package/bluetooth-serial-port
+
+    client_obj.findSerialPortChannel(target_mac_address, (channel) => {
+        // directly connect, skip the `find` (.on found evt) sequence
+
+        client_obj.connect(target_mac_address, channel, () => {
+            is_connected_flag = true;
+            elBluetooth.innerHTML = `Connected to ${target_mac_address}::${channel}`;
+
+            // send data to it by `.write()`
+            client_obj.write(
+                Buffer.from(() => {
+
+                }, 'utf-8'), (err, bytes_written) => {
+                    if (err) console.err(err);
+                }
+            );
+
+            client_obj.on('data', (buffer) => {
+                const msg = buffer.toString().trim();
+
+                // NOTE: a cc of `socket_connect()` `on('data')` evt 
+                try {
+                    if (msg.startsWith("CMD_MODE_D")) {
+                        const distance = parseFloat(msg.split("#")[2]);
+                        if (!isNaN(distance)) {
+                            elObstacle.textContent = distance.toFixed(2);
+                        }
+                    } else if (msg.startsWith("CMD_MODE_T")) {
+                        const cpu_temperature = parseFloat(msg.split('#')[1]);
+                        elTemperature.textContent = cpu_temperature.toFixed(2);
+                    } else if (msg.startsWith("CMD_MODE_C")) {
+                        const cpu_load = parseFloat(msg.split('#')[1]);
+                        elCPULoad.textContent = cpu_load.toFixed(1);
+                    }
+                } catch (err) {
+                    console.warn("⚠️ Failed to parse message:", msg);
+                }
+            });
+        }, (err) => {
+            teardown();
+            elBluetooth.textContent = "Connection failed: " + err.message;
+        });
+
+        client_obj.close(() => {
+            teardown();
+            elBluetooth.textContent = "Disconnected";
+        });
+    });
+
+    // client_obj.inquire();
+}
 
 // -------------------- Connect to TCP Server --------------------
 function socket1_connect(ip) {
@@ -186,7 +274,9 @@ window.onload = () => {
     elTemperature = document.getElementById("temperature");
     elCpuLoad = document.getElementById("cpu_load");
 
-    socket1_connect(server_addr);
+    assert.ok(CONNECTION_TYPE_ENUM.has(CONNECTION_TYPE), `window.onload()::\"${CONNECTION_TYPE}\" is an invalid connection type`);
+    (CONNECTION_TYPE == "ip") ? socket1_connect(server_addr) : bt_connect(BLUETOOTH_MAC_ADDRESS);
+
     document.onkeydown = updateKey;
     document.onkeyup = resetKey;
 };
